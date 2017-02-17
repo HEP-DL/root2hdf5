f1550c129808e354f37020056d741d01b7db0b9f
Kevin Wierman <kwierman@gmail.com>
2017-02-17 11:15:20 -0600

    Fixing imports

--- root2hdf5/data_types/larcv/__init__.py
+++ root2hdf5/data_types/larcv/__init__.py
@@ -1,1 +1,5 @@
 from root2hdf5.data_types.larcv.tpcimage import TPCImage
+from root2hdf5.data_types.larcv.channelstatus import ChannelStatusData
+from root2hdf5.data_types.larcv.pmt import PMTData
+from root2hdf5.data_types.larcv.roi import ROIData
+from root2hdf5.data_types.larcv.segment import SegmentData
