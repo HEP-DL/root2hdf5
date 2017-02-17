7387dec89f1185325d63e5e10f19cc2dacc0f13c
Kevin Wierman <kwierman@gmail.com>
2017-02-17 11:00:44 -0600

    Changed the variadic parameter setting

--- root2hdf5/cli.py
+++ root2hdf5/cli.py
@@ -5,13 +5,14 @@ import logging
 import sys
 
 @click.command()
-def main(args=None):
+@click.argument('src', nargs=-1)
+def main(files=None):
     logging.basicConfig(level=logging.INFO)
-    if args == None:
-      click.echo("usage: root2hdf5 filename.root [filename.root [...]]")
+    if files == None:
+      click.echo("usage: root2hdf5 --src filename.root [filename.root [...]]")
       sys.exit(0)
     from root2hdf5.converters.larcv_file import LArCVFile
-    LArCVFile(args).setup().go()
+    LArCVFile(files).setup().go()
 
 if __name__ == "__main__":
     main()
